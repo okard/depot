6fb64b929866ebb4f3d2abda4d90bbf384677912
okard <d4rkdragon@gmail.com>
2012-10-22 09:05:09 +0200

    BoardGame: Script create tex file

--- Txt.BoardGame/tools/create_sv.sh
+++ Txt.BoardGame/tools/create_sv.sh
@@ -2,13 +2,27 @@
 
 
 # create single view svgs
-./gentokensvg.py sv tokendb.txt "generated/token_{0}_{1}.svg"
+mkdir -v -p generated/token
+./gentokensvg.py sv tokendb.txt "generated/token/token_{0}_{1}.svg"
 
 # convert each single view svg to pdf
-for f in generated/token_*.svg; do 
+for f in generated/token/token_*.svg; do 
     echo "Create pdf for $f ..."; 
 
     # rules/generated/token/
-    inkscape -z -f $f -D --export-dpi=300 --export-pdf ${f%.svg}.pdf
+    inkscape -z -f $f -D --export-dpi=300 --export-pdf ../rules/${f%.svg}.pdf
+done
+
+
+# create tex 
+
+echo "Create tex list"
+echo "%generated" > ../rules/generated/tokendb.tex
+for f in ../rules/generated/token/token_*.pdf; do 
+
+    #\includegraphics[scale=3]{generated/token/token_120010011_220010023.pdf} & • & • \\ 
+    echo -n "\includegraphics[scale=3]{" >> ../rules/generated/tokendb.tex
+    echo -n ${f:9} >> ../rules/generated/tokendb.tex
+    echo "} & • & • \\\\" >> ../rules/generated/tokendb.tex
 done
 
