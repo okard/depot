6513dd02d85f1258698a6d72f3a8e037e203160a
okard <d4rkdragon@gmail.com>
2012-11-25 13:16:55 +0100

    Script creating home structure

---
+++ Shell.Scripts/CreateHomeDirectories.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+mkdir ~/20.Dev
+mkdir ~/20.Dev/10.Projects
+
+mkdir ~/40.Studies
+
+mkdir ~/50.Mp3
+mkdir ~/51.Movies
+
+mkdir ~/90.Downloads
+mkdir ~/91.Other
+mkdir ~/93.Builds
+mkdir ~/95.Mount
