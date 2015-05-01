084b1cd1a7e224a5f3b15dd8e195d45a2b80325c
okard <d4rkdragon@gmail.com>
2015-05-01 17:39:39 +0200

    rust.spheregen: update

--- Rust.SphereGen/src/main.rs
+++ Rust.SphereGen/src/main.rs
@@ -20,6 +20,7 @@ struct Point
 
 fn main()
 {
+	//when arguments available use first one for config file
 	let mut conf_file = File::open("spheregen.config.toml").unwrap();
 
 	let mut conf_content = "".to_string();
@@ -27,6 +28,7 @@ fn main()
 	let config: toml::Value = conf_content.parse().unwrap();
 	
 	let count = config.lookup("general.count").unwrap().as_integer().unwrap();
+	let radius = config.lookup("general.radius").unwrap().as_integer().unwrap();
 	
 	let x_min = config.lookup("x.min").unwrap().as_integer().unwrap();
 	let x_max = config.lookup("x.max").unwrap().as_integer().unwrap();
@@ -42,12 +44,20 @@ fn main()
 	
 	let mut outfile = File::create("output.csv").unwrap();
 	
-	for _ in 0..count
+	let mut i = 0;
+	
+	while i < count
 	{
 		let x = rnd.gen_range(x_min, x_max);
 		let y = rnd.gen_range(y_min, y_max);
 		let z = rnd.gen_range(z_min, z_max);
 		
-		write!(&mut outfile, "{}, {}, {}\n", x, y, z);
+		//calculate distance from center (0,0) and check if point is in circle
+		if radius > 0 
+		&& radius.pow(2) <= x.pow(2) + y.pow(2) + z.pow(2) 
+		{
+			write!(&mut outfile, "{}, {}, {}\n", x, y, z);
+			i += 1;
+		}
 	}
 }
